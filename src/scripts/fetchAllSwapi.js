85a6b44774b2c2f90cc779a3f56fdeadc90450cb
MonMon201 <danyavasilinenko213@gmail.com>
2020-11-02 05:24:23 +0200

    feat: add fetch all swapi function

---
+++ src/scripts/fetchAllSwapi.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const swapi = require('swapi-node')
+
+const fetchAllSwapi = (acc, link) => {
+    return new Promise((resolve, reject) => {
+      swapi.get(link).then((data) => {
+        acc = acc.concat(data.results);
+        if(data.next){
+          const num = parseInt(link.slice(link.length-1))+1;
+          link = link.slice(0, link.length-1) + num;
+          resolve(fetchAllSwapi(acc, link));
+        } else {
+          resolve(acc);
+        }
+      }).catch((err)=>{
+        throw err;
+      })
+    })
+}
+
+module.exports = {
+    fetchAllSwapi,
+}
